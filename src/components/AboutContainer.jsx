1a1f3badc380b235f301f34153227400f12dd9ae
alveselivelton <eliveltonsa92@gmail.com>
2022-12-11 09:08:07 -0300

    correção-de-texto

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -11,11 +11,11 @@ const AboutContainer = () => {
       <p>
         Meus principais hobbies sempre foram a prática de atividades físicas,
         mas hoje, posso afirmar que passar horas codando se tornou tão bom
-        quanto. Espero transformar esse novo hobbie em uma carreira
-        profissional, visando sempre contribuir da melhor forma possível na
-        evolução da empresa, além de absorver ao máximo toda a experiência
-        adquirida através do convívio com outros programadores, a fim de me
-        tornar um profissional cada vez melhor.
+        quanto. Espero transformar esse novo hobby em uma carreira profissional,
+        visando sempre contribuir da melhor forma possível na evolução da
+        empresa, além de absorver ao máximo toda a experiência adquirida através
+        do convívio com outros programadores, a fim de me tornar um profissional
+        cada vez melhor.
       </p>
     </section>
   );
